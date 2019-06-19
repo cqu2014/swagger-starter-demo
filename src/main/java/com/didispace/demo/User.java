b7a6da638c0ebf6834361b2214dc644263333d8b
cqu2014 <286931986@qq.com>
2019-06-19 19:37:30 +0800

    消除Illegal DefaultValue null for parameter type integer

--- src/main/java/com/didispace/demo/User.java
+++ src/main/java/com/didispace/demo/User.java
@@ -7,22 +7,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.*;
+import java.io.Serializable;
 
+/**
+ * @author dell
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ApiModel("用户基本信息")
-public class User {
+@ApiModel(value = "用户基本信息",description = "用户基本信息")
+public class User implements Serializable {
+
+    @ApiModelProperty(value = "ID")
+    private Long id;
 
     @ApiModelProperty("姓名")
     @Size(max = 20)
     private String name;
-    @ApiModelProperty("年龄")
+
+    @ApiModelProperty(value = "年龄")
     @Max(150)
     @Min(1)
-    private Integer age;
+    private Long age;
+
     @NotNull
     private String address;
+
     @Pattern(regexp = "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$")
     private String email;
 
--- src/main/java/com/didispace/demo/UserController.java
+++ src/main/java/com/didispace/demo/UserController.java
@@ -3,6 +3,8 @@ package com.didispace.demo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.util.Json;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -10,29 +12,44 @@ import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author dell
+ */
 @Api(tags="用户管理")
 @RestController
+@Slf4j
 public class UserController {
 
     @ApiOperation("创建用户")
     @PostMapping("/users")
     public User create(@RequestBody @Valid User user) {
+        log.info("create:input data [{}]", Json.pretty(user));
         return user;
     }
 
     @ApiOperation("用户详情")
     @GetMapping("/users/{id}")
     public User findById(@PathVariable Long id) {
-        return new User("bbb", 21, "上海", "aaa@bbb.com");
+        return new User(id,"bbb", 51L, "上海", "aaa@bbb.com");
     }
 
     @ApiOperation("用户列表")
     @GetMapping("/users")
-    public List<User> list(@ApiParam("查看第几页") @RequestParam int pageIndex,
-                           @ApiParam("每页多少条") @RequestParam int pageSize) {
+    public List<User> list(@ApiParam(value = "查看第几页",example = "0") @RequestParam Integer pageIndex,
+                           @ApiParam(value = "每页多少条",example = "1") @RequestParam Integer pageSize) {
         List<User> result = new ArrayList<>();
-        result.add(new User("aaa", 50, "北京", "aaa@ccc.com"));
-        result.add(new User("bbb", 21, "广州", "aaa@ddd.com"));
+        result.add(new User(1L,"aaa", 123L, "北京", "aaa@ccc.com"));
+        result.add(new User(2L,"bbb", 123L, "广州", "aaa@ddd.com"));
+        return result;
+    }
+
+    @ApiOperation("用户列表")
+    @GetMapping("/users/all")
+    public List<User> lists(@ApiParam(value = "查看第几页") @RequestParam Integer pageIndex,
+                           @ApiParam(value = "每页多少条") @RequestParam Integer pageSize) {
+        List<User> result = new ArrayList<>();
+        result.add(new User(2L,"bbb", 123L, "广州", "aaa@ddd.com"));
+        result.add(new User(1L,"aaa", 123L, "北京", "aaa@ccc.com"));
         return result;
     }
 
